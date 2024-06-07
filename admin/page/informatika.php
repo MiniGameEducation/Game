71e3a610c2a271d37d568f710434ddae69eb2570
Ability1212 <ubaidilahmahendra1234@gmail.com>
2024-06-07 21:33:47 +0700

    fix konten

--- admin/page/informatika.php
+++ admin/page/informatika.php
@@ -1,3 +1,37 @@
-<div class="user-table">
-    infor
-</div>
+<main class="user-table">
+    <section class="table-body">
+        <h1>Pertanyaan</h1>
+        <table>
+            <tr>
+                <td><input type="text" class="text1" id=""></input></td>
+            </tr>
+        </table>
+        <h1>Jawaban</h1>
+        <table>
+            <tbody>
+                <tr>
+                    <td><input type="text" class="text" id=""></input></td>
+                </tr>
+                <tr>
+                    <td><input type="text" class="text" id=""></input></td>
+                </tr>
+                <tr>
+                    <td><input type="text" class="text" id=""></td>
+                </tr>
+                <tr>
+                    <td><input type="text" class="text" id=""></input></td>
+                </tr>
+            </tbody>
+        </table>
+    </section>
+    <ul class="pagination">
+        </li>
+        <li class="page-item active"><a class="page-link" href="bahasa.html">1</a></li>
+        <li class="page-item"><a class="page-link" href="bahasa2.html">2</a></li>
+        <li class="page-item"><a class="page-link" href="bahasa3.html">3</a></li>
+        <li class="page-item"><a class="page-link" href="bahasa4.html">4</a></li>
+        <li class="page-item"><a class="page-link" href="bahasa5.html">5</a></li>
+        <li class="page-item"><a class="page-link" href="bahasa5-2.html">6</a></li>
+        </li>
+    </ul>
+</main>
